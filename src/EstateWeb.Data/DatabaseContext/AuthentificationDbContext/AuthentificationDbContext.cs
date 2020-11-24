47f3e0bc5cb017c08604c238abb64fd9f3aa5158
Dera Ezenwa <dera9q@outlook.com>
2020-11-24 01:18:10 +0100

    added authentification db context class

--- src/EstateWeb.Data/DatabaseContext/AuthentificationDbContext/AuthentificationDbContext.cs
+++ src/EstateWeb.Data/DatabaseContext/AuthentificationDbContext/AuthentificationDbContext.cs
@@ -1,8 +1,15 @@
+using System.Net.Mime;
+using EstateWeb.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace EstateApp.Data.DatabaseContext.AuthentificationDbContext
 {
-    
-    public class AuthentificationDbContext
+    public class AuthentificationDbContext : IdentityDbContext<ApplicationUser>
     {
-        
+        public AuthentificationDbContext(DbContextOptions<AuthentificationDbContext>options) : base(options)
+           {
+
+           }
     }
 }
---
+++ src/EstateWeb.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace EstateWeb.Data.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
